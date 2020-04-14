bd47e3396f4276a10c236efad5af7b3fae5e9a23
vbty <hucvbty@gmail.com>
2020-04-14 19:49:23 +0800

    upload

---
+++ SampleRpc/SampleRpcClient/SampleRpcClient.c
@@ -0,0 +1,63 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include "Schedsvc_h.h"
+//#include "SampleRpc_h.h"
+
+int main()
+{
+    RPC_STATUS status;
+    RPC_WSTR BindString;
+    RPC_BINDING_HANDLE BindHandle;
+
+    status = RpcStringBindingComposeW(
+        NULL,
+        (RPC_WSTR)L"ncalrpc",
+        NULL,
+        (RPC_WSTR)L"ubpmtaskhostchannel",
+        NULL,
+        &BindString);
+
+    if (status) {
+        printf("RpcStringBindingComposeW failed\n");
+        exit(status);
+    }
+
+    status = RpcBindingFromStringBindingW(
+        BindString,
+        &BindHandle);
+
+    if (status) {
+        printf("RpcBindingFromStringBindingW failed\n");
+        exit(status);
+    }
+
+    RpcTryExcept
+    {
+        while (1)
+        {
+            Proc13(BindHandle,
+                L"\\CreateExplorerShellUnelevatedTask",
+                0);
+            system("pause");
+        }
+    }
+    RpcExcept(1)
+    {
+        printf("RPC Exception %d/n", RpcExceptionCode());
+    }
+    RpcEndExcept
+
+
+    RpcBindingFree(&BindHandle);
+
+}
+
+void __RPC_FAR* __RPC_USER midl_user_allocate(size_t len)
+{
+    return(malloc(len));
+}
+
+void __RPC_USER midl_user_free(void __RPC_FAR* ptr)
+{
+    free(ptr);
+}
---
+++ SampleRpc/SampleRpcServer/SampleRpcServer.c
@@ -0,0 +1,63 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include "SampleRpc_h.h"
+
+void HelloProc(
+	/* [in] */ handle_t IDL_handle,
+	/* [string][in] */ unsigned char* pszString)
+{
+	printf("%s/n", pszString);
+}
+
+
+int main()
+{
+	RPC_STATUS status;
+
+	status = RpcServerUseProtseqEpW(
+		(RPC_WSTR)L"ncalrpc",
+		RPC_C_PROTSEQ_MAX_REQS_DEFAULT,
+		(RPC_WSTR)L"RPC-ALPC-Server",
+		NULL);
+
+	if (status)
+	{
+		printf("Error RpcServerUseProtseqEpW: %d\n", status);
+		exit(status);
+	}
+
+	status = RpcServerRegisterIf(
+		hello_v1_0_s_ifspec,
+		NULL,
+		NULL);
+
+
+	if (status)
+	{
+		printf("Error RpcServerRegisterIf: %d\n", status);
+		exit(status);
+	}
+
+	status = RpcServerListen(
+		1,
+		RPC_C_LISTEN_MAX_CALLS_DEFAULT,
+		FALSE);
+
+
+	if (status) {
+		printf("Error RpcServerListen: %d\n", status);
+		exit(status);
+	}
+	else
+		puts("Server is OK");
+}
+
+void __RPC_FAR* __RPC_USER midl_user_allocate(size_t len)
+{
+	return(malloc(len));
+}
+
+void __RPC_USER midl_user_free(void __RPC_FAR* ptr)
+{
+	free(ptr);
+}
